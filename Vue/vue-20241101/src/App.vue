36298b51fcaeb197020b2e56487f8c61ea55c176
JamesHa0 <2258693275@qq.com>
2024-11-15 14:32:56 +0800

    2024.11.01

---
+++ Vue/vue-20241101/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+
+  <body>
+    <div id="app">
+      <p v-for="book in bookArr" :key="book.bookId">
+        ​{{ book.bookId }},{{ book.bookName }},{{ book.author }}<br>
+      </p>
+    </div>
+  </body>
+</template>
+<script src="https://unpkg.com/vue@next"></script>
+<script>
+const bookArr = reactive([
+      {
+        bookId: 1,
+        bookName: '《三国演义》',
+        author: '罗贯中'
+      },
+      {
+        bookId: 2,
+        bookName: '《西游记》',
+        author: '吴承恩'
+      },
+      {
+        bookId: 3,
+        bookName: '《水浒传》',
+        author: '施耐庵'
+      }
+    ]);
+</script>
---
+++ Vue/vue-20241101/src/router/index.ts
@@ -0,0 +1,13 @@
+
+
+const router = createRouter({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    }
+  ]
+})
+
+export default router
