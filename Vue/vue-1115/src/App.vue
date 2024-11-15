e0b39c15c70c545bbaffcdc3f0cc32e4f6a705be
JamesHa0 <2258693275@qq.com>
2024-11-15 15:03:50 +0800

    2024.11.15

---
+++ Vue/vue-1115/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>首页</h1>
+  <button @click="test">发送请求</button>
+</template>
+
+<script setup lang="ts">
+import axios from 'axios'
+
+function test() {
+  axios.get('http://localhost:8080/demo1114/first').then(function (response) {
+    console.log(response.data[0]);
+    console.log(response.data[1]);
+
+  })
+}
+</script>
+
+<style scoped></style>
